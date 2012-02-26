72d62cd668889d19cf8e395222581ecaedc2738b
David Elam <delam@uptake.com>
2012-02-26 16:05:37 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitsucker::Application.config.secret_token = 'fae220ba9af7e693fbc02d4042124942676b526332d31a12cb81757e73bf7704a9ed0a4183ee38cd3d023ee0c670fbd37a2053d11301b9e738c3b3253cbafe00'
